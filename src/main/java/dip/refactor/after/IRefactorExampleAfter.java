d89aaeea5993b2bae5d063f7b2ebc3343aa5b039
Halevi, Asaf (ah864q) <ah864q@intl.att.com>
2020-10-13 11:37:53 +0300

    Improve interfaces example

--- src/main/java/dip/refactor/after/IRefactorExampleAfter.java
+++ src/main/java/dip/refactor/after/IRefactorExampleAfter.java
@@ -1,5 +1,11 @@
 package dip.refactor.after;
 
 public interface IRefactorExampleAfter {
+    void eat();
 
+    void swim();
+
+    void run();
+
+    void fly();
 }
--- src/main/java/dip/refactor/after/RefactorExampleAfter.java
+++ src/main/java/dip/refactor/after/RefactorExampleAfter.java
@@ -2,18 +2,22 @@ package dip.refactor.after;
 
 public class RefactorExampleAfter implements IRefactorExampleAfter {
 
+	@Override
 	public void eat() {
 		//DO SOMETHING
 	}
 
+	@Override
 	public void swim(){
 		//DO SOMETHING
 	}
 
+	@Override
 	public void run(){
 		//DO SOMETHING
 	}
 
+	@Override
 	public void fly(){
 		//DO SOMETHING
 	}
