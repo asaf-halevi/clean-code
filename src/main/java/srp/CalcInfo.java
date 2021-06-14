1968bb260d58208681bff4758a6257f296ce640a
Halevi, Asaf Eytan (ah864q) <ah864q@intl.att.com>
2021-06-14 19:11:47 +0300

    [000000] fix formatting

--- src/main/java/srp/CalcInfo.java
+++ src/main/java/srp/CalcInfo.java
@@ -15,45 +15,45 @@ public class CalcInfo {
     private int b;
     private char action;
 
-    public CalcInfo (int a, int b, char action) throws InvalidAlgorithmParameterException {
-        super ();
+    public CalcInfo(int a, int b, char action) throws InvalidAlgorithmParameterException {
+        super();
         this.a = a;
         this.b = b;
-        this.setAction (action);
+        this.setAction(action);
     }
 
-    public int getA () {
+    public int getA() {
 
         return a;
     }
 
-    public void setA (int a) {
+    public void setA(int a) {
 
         this.a = a;
     }
 
-    public int getB () {
+    public int getB() {
         return b;
     }
 
-    public void setB (int b) {
+    public void setB(int b) {
         this.b = b;
     }
 
-    public char getAction () {
+    public char getAction() {
         return action;
     }
 
-    public void setAction (char action) throws InvalidAlgorithmParameterException {
-        if (VALID_ACTIONS.contains (String.valueOf (action))) {
+    public void setAction(char action) throws InvalidAlgorithmParameterException {
+        if (VALID_ACTIONS.contains(String.valueOf(action))) {
             this.action = action;
         } else {
-            throw new InvalidAlgorithmParameterException (action + " is not a valid action!");
+            throw new InvalidAlgorithmParameterException(action + " is not a valid action!");
         }
     }
 
     @Override
-    public String toString () {
+    public String toString() {
         return "CalcInfo [a=" + a + ", b=" + b + ", action=" + action + "]";
     }
 }
