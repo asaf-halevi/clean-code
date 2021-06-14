c8fcc6318bfdc9c3fbb8dd4f5ad00aec0c4a130a
Halevi, Asaf Eytan (ah864q) <ah864q@intl.att.com>
2021-06-14 18:50:51 +0300

    [000000] fix formatting

--- src/main/java/srp/SingleResponsibilityPrincipleAfter.java
+++ src/main/java/srp/SingleResponsibilityPrincipleAfter.java
@@ -8,78 +8,78 @@ public class SingleResponsibilityPrincipleAfter {
     public void performActions() {
         CalcInfo calcInfo = null;
         try {
-            calcInfo = getInfo ();
+            calcInfo = getInfo();
         } catch (InvalidAlgorithmParameterException e) {
-            e.printStackTrace ();
+            e.printStackTrace();
             return;
         }
 
-        int result = calcResult (calcInfo);
+        int result = calcResult(calcInfo);
 
-        System.out.println (getResultAsString (calcInfo, result));
+        System.out.println(getResultAsString(calcInfo, result));
     }
 
-    private CalcInfo getInfo () throws InvalidAlgorithmParameterException {
-        Scanner sc = new Scanner (System.in);
-        System.out.print ("first number: ");
-        int a = sc.nextInt ();
-        System.out.print ("second number: ");
-        int b = sc.nextInt ();
-        System.out.print ("action [+, -, *, /, %]: ");
-        String action = sc.next ();
-        sc.close ();
+    private CalcInfo getInfo() throws InvalidAlgorithmParameterException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("first number: ");
+        int a = sc.nextInt();
+        System.out.print("second number: ");
+        int b = sc.nextInt();
+        System.out.print("action [+, -, *, /, %]: ");
+        String action = sc.next();
+        sc.close();
 
-        return new CalcInfo (a, b, action.charAt (0));
+        return new CalcInfo(a, b, action.charAt(0));
     }
 
-    private int calcResult (CalcInfo calcInfo) {
+    private int calcResult(CalcInfo calcInfo) {
         int result = 0;
-        switch (calcInfo.getAction ()) {
+        switch (calcInfo.getAction()) {
             case CalcInfo.ADD:
-                result = add (calcInfo.getA (), calcInfo.getB ());
+                result = add(calcInfo.getA(), calcInfo.getB());
                 break;
             case CalcInfo.SUBTRACT:
-                result = subtract (calcInfo.getA (), calcInfo.getB ());
+                result = subtract(calcInfo.getA(), calcInfo.getB());
                 break;
             case CalcInfo.MULTIPLY:
-                result = multiply (calcInfo.getA (), calcInfo.getB ());
+                result = multiply(calcInfo.getA(), calcInfo.getB());
                 break;
             case CalcInfo.DIVIDE:
-                result = divide (calcInfo.getA (), calcInfo.getB ());
+                result = divide(calcInfo.getA(), calcInfo.getB());
                 break;
             case CalcInfo.MODULUS:
-                result = modulus (calcInfo.getA (), calcInfo.getB ());
+                result = modulus(calcInfo.getA(), calcInfo.getB());
                 break;
         }
         return result;
     }
 
-    protected String getResultAsString (CalcInfo calcInfo, int result) {
-        return "\n" + calcInfo.getA () + " " + calcInfo.getAction () + " " + calcInfo.getB () + " = " + result;
+    protected String getResultAsString(CalcInfo calcInfo, int result) {
+        return "\n" + calcInfo.getA() + " " + calcInfo.getAction() + " " + calcInfo.getB() + " = " + result;
     }
 
-    protected int subtract (int a, int b) {
+    protected int subtract(int a, int b) {
         return a - b;
     }
 
-    protected int add (int a, int b) {
+    protected int add(int a, int b) {
         return a + b;
     }
 
-    protected int multiply (int a, int b) {
+    protected int multiply(int a, int b) {
         return a * b;
     }
 
-    protected int divide (int a, int b) {
+    protected int divide(int a, int b) {
         return a / b;
     }
 
-    protected int modulus (int a, int b) {
+    protected int modulus(int a, int b) {
         return a % b;
     }
 
-    public static void main (String[] args) throws InvalidAlgorithmParameterException {
-        SingleResponsibilityPrincipleAfter calculator = new SingleResponsibilityPrincipleAfter ();
+    public static void main(String[] args) throws InvalidAlgorithmParameterException {
+        SingleResponsibilityPrincipleAfter calculator = new SingleResponsibilityPrincipleAfter();
         calculator.performActions();
     }
 }
--- src/main/java/srp/SingleResponsibilityPrincipleAfterTest.java
+++ src/main/java/srp/SingleResponsibilityPrincipleAfterTest.java
@@ -9,53 +9,53 @@ import org.junit.Test;
 
 public class SingleResponsibilityPrincipleAfterTest {
 
-	private static SingleResponsibilityPrincipleAfter calculator;
+    private static SingleResponsibilityPrincipleAfter calculator;
 
-	@BeforeClass
-	public static void beforeTests() {
-		calculator = new SingleResponsibilityPrincipleAfter();
-	}
+    @BeforeClass
+    public static void beforeTests() {
+        calculator = new SingleResponsibilityPrincipleAfter();
+    }
 
-	@Test
-	public void addTest() {
-		int actual = calculator.add(9 ,5);
-		int expected = 9 + 5;
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void addTest() {
+        int actual = calculator.add(9, 5);
+        int expected = 9 + 5;
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void subtractTest() {
-		int actual = calculator.subtract(9 ,5);
-		int expected = 9 - 5;
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void subtractTest() {
+        int actual = calculator.subtract(9, 5);
+        int expected = 9 - 5;
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void mutlitplyTest() {
-		int actual = calculator.multiply(9 ,5);
-		int expected = 9 * 5;
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void mutlitplyTest() {
+        int actual = calculator.multiply(9, 5);
+        int expected = 9 * 5;
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void divideTest() {
-		int actual = calculator.divide(9 ,5);
-		int expected = 9 / 5;
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void divideTest() {
+        int actual = calculator.divide(9, 5);
+        int expected = 9 / 5;
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void modulusTest() {
-		int actual = calculator.modulus(9 ,5);
-		int expected = 9 % 5;
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void modulusTest() {
+        int actual = calculator.modulus(9, 5);
+        int expected = 9 % 5;
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void getResultAsStringTest() throws InvalidAlgorithmParameterException {
-		CalcInfo calcInfo = new CalcInfo(1, 1, CalcInfo.ADD);
-		String actual = calculator.getResultAsString(calcInfo, 2);
-		String expected = "\n1 + 1 = 2";
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void getResultAsStringTest() throws InvalidAlgorithmParameterException {
+        CalcInfo calcInfo = new CalcInfo(1, 1, CalcInfo.ADD);
+        String actual = calculator.getResultAsString(calcInfo, 2);
+        String expected = "\n1 + 1 = 2";
+        assertEquals(expected, actual);
+    }
 }
