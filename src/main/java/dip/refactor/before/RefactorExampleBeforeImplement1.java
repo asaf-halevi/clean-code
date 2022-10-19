b42e77fd6f10b645e5201ca92d7ec506c8f774bb
Halevi, Asaf <asaf.halevi@gmail.com>
2022-10-19 17:38:01 +0300

    [0000] delete code

---
+++ src/main/java/dip/refactor/before/RefactorExampleBeforeImplement1.java
@@ -0,0 +1,24 @@
+package dip.refactor.before;
+
+public class RefactorExampleBeforeImplement1 implements RefactorExampleBefore {
+
+    @Override
+    public void eat() {
+        //DO SOMETHING
+    }
+
+    @Override
+    public void swim() {
+        //DO SOMETHING
+    }
+
+    @Override
+    public void run() {
+        //DO SOMETHING
+    }
+
+    @Override
+    public void fly() {
+        //DO SOMETHING
+    }
+}
