06a61e1a67fac97b9a38393988d76bd9e9f104e6
Halevi, Asaf <asaf.halevi@gmail.com>
2024-04-29 19:50:18 +0300

    [0000] add comments

--- src/main/java/lsp/Rectangle.java
+++ src/main/java/lsp/Rectangle.java
@@ -2,10 +2,6 @@ package lsp;
 
 public class Rectangle {
 
-    protected final static float MAX_PERIMETER_DEFINED_BY_CLIENT = 35;
-    protected final static String MAX_PERIMETER_DEFINED_BY_CLIENT_ERR =
-            "Client will not accept perimeters greater than " + MAX_PERIMETER_DEFINED_BY_CLIENT;
-
     private float length;
     private float width;
 
@@ -14,17 +10,8 @@ public class Rectangle {
         setWidth(width);
     }
 
-    // return the perimeter after verification
     public float getPerimeter() {
-        return verifyPerimeter(2 * length + 2 * width);
-    }
-
-    // perimeter must not exceed a given max size
-    protected float verifyPerimeter(float perimeter) {
-        if (perimeter > MAX_PERIMETER_DEFINED_BY_CLIENT) {
-            throw new ArithmeticException(MAX_PERIMETER_DEFINED_BY_CLIENT_ERR);
-        }
-        return perimeter;
+        return 2 * length + 2 * width;
     }
 
     public float getArea() {
@@ -35,7 +22,6 @@ public class Rectangle {
         return length;
     }
 
-    // length must be a positive number
     public void setLength(float length) {
         if (length <= 0) {
             throw new ArithmeticException("length must be positive");
@@ -47,7 +33,6 @@ public class Rectangle {
         return width;
     }
 
-    // width must be a positive number
     public void setWidth(float width) {
         if (width <= 0) {
             throw new ArithmeticException("width must be positive");
