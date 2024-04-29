d69f3304bd20d9e74c7d969aff3371da4a425607
Halevi, Asaf <asaf.halevi@gmail.com>
2024-04-29 19:26:44 +0300

    [0000] add comments

--- src/main/java/lsp/Rectangle.java
+++ src/main/java/lsp/Rectangle.java
@@ -3,7 +3,8 @@ package lsp;
 public class Rectangle {
 
     protected final static float MAX_PERIMETER_DEFINED_BY_CLIENT = 35;
-    protected final static String MAX_PERIMETER_DEFINED_BY_CLIENT_ERR = "Client will not accept perimeters greater than " + MAX_PERIMETER_DEFINED_BY_CLIENT;
+    protected final static String MAX_PERIMETER_DEFINED_BY_CLIENT_ERR =
+            "Client will not accept perimeters greater than " + MAX_PERIMETER_DEFINED_BY_CLIENT;
 
     private float length;
     private float width;
@@ -13,18 +14,20 @@ public class Rectangle {
         setWidth(width);
     }
 
-    public float getPerimeter(){
+    // return the perimeter after verification
+    public float getPerimeter() {
         return verifyPerimeter(2 * length + 2 * width);
     }
 
-    protected float verifyPerimeter(float perimeter){
-        if (perimeter > MAX_PERIMETER_DEFINED_BY_CLIENT){
+    // perimeter must not exceed a given max size
+    protected float verifyPerimeter(float perimeter) {
+        if (perimeter > MAX_PERIMETER_DEFINED_BY_CLIENT) {
             throw new ArithmeticException(MAX_PERIMETER_DEFINED_BY_CLIENT_ERR);
         }
         return perimeter;
     }
 
-    public float getArea(){
+    public float getArea() {
         return length * width;
     }
 
@@ -32,8 +35,9 @@ public class Rectangle {
         return length;
     }
 
+    // length must be a positive number
     public void setLength(float length) {
-        if (length <=0){
+        if (length <= 0) {
             throw new ArithmeticException("length must be positive");
         }
         this.length = length;
@@ -43,8 +47,9 @@ public class Rectangle {
         return width;
     }
 
+    // width must be a positive number
     public void setWidth(float width) {
-        if (width <=0){
+        if (width <= 0) {
             throw new ArithmeticException("width must be positive");
         }
         this.width = width;
