fb839b55959ccba8d87cc7dfd0486f5ed2519ff9
Halevi, Asaf <asaf.halevi@gmail.com>
2024-04-29 19:48:52 +0300

    [0000] add comments

--- src/main/java/lsp/RectanglesManager.java
+++ src/main/java/lsp/RectanglesManager.java
@@ -6,13 +6,17 @@ import java.util.List;
 public class RectanglesManager {
     public static void main(String[] args) {
         List<Rectangle> rectangles = new ArrayList<>();
-        //Parent
+
         rectangles.add(new Rectangle(3, 4));
         rectangles.add(new Rectangle(6, 7));
 
-        //replace with BadSquare / GoodSquare (and replace constructor)
-        //rectangles.add(new Rectangle(10, 10));//valid parameter will NOT be accepted
-        //rectangles.add(new Rectangle(0, 0));//invalid parameter will be accepted
+        // replace with bad/good square
+        rectangles.add(new Rectangle(10, 10));
+        try {
+            rectangles.add(new Rectangle(0, 0));
+        } catch (ArithmeticException e) {
+            System.out.println("Exception, as expected: " + e.getMessage());
+        }
 
         for (Rectangle r : rectangles) {
             System.out.println(r + " | perimeter: " + r.getPerimeter() + " | area: " + r.getArea());
